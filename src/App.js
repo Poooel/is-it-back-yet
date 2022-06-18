2ba808bd7d08ec7b7c677ac0be3ecaa1aa5a4bb8
Paul Queruel <paul.queruel@protonmail.com>
2022-06-19 00:34:08 +0100

    reactified and removed nextjs

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import { QueryClient, QueryClientProvider } from 'react-query'
+import Home from './Home';
+
+function App() {
+  const queryClient = new QueryClient({
+    defaultOptions: {
+      queries: {
+        retry: false,
+        refetchOnWindowFocus: false
+      }
+    }
+  })
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Home/>
+    </QueryClientProvider>
+  );
+}
+
+export default App;
--- src/Home.js
+++ src/Home.js
@@ -1,6 +1,6 @@
 import { useQuery } from "react-query"
 
-export default function Home() {
+function Home() {
   const { isLoading, data, refetch, isRefetching } = useQuery(
     'check-website', 
     () => fetch('/api/check').then(res => res.json())
@@ -16,7 +16,7 @@ export default function Home() {
             </p>
             {(data.status || data.lastStatusChecked) === "OPEN" && (
               <div className="bg-slate-200 rounded-lg p-2 m-4 hover:underline hover:text-blue-500 flex justify-center">
-                <a href="https://www.oat.ie/" target="_blank">
+                <a href="https://www.oat.ie/" target="_blank" rel="noreferrer">
                   Order on Oat.ie
                 </a>
                 <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 mx-1" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
@@ -29,8 +29,8 @@ export default function Home() {
             </p>
             <button className="mt-4 px-4 py-2" type="button" onClick={refetch}>
               <div className={ isRefetching ? "animate-spin" : "" }>
-                <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 -scale-x-100" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
-                  <path stroke-linecap="round" stroke-linejoin="round" d="M4 4v5h.582m15.356 2A8.001 8.001 0 004.582 9m0 0H9m11 11v-5h-.581m0 0a8.003 8.003 0 01-15.357-2m15.357 2H15" />
+                <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 -scale-x-100" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
+                  <path strokeLinecap="round" strokeLinejoin="round" d="M4 4v5h.582m15.356 2A8.001 8.001 0 004.582 9m0 0H9m11 11v-5h-.581m0 0a8.003 8.003 0 01-15.357-2m15.357 2H15" />
                 </svg>
               </div>
             </button>
@@ -43,3 +43,5 @@ export default function Home() {
     </div>
   )
 }
+
+export default Home;
