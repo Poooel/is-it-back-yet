d56469e29181c51a096812b45144589691b4a94e
Paul Queruel <paul.queruel@protonmail.com>
2022-06-18 17:39:42 +0100

    test first draft of function with kv

--- functions/api/check.js
+++ functions/api/check.js
@@ -8,6 +8,39 @@ export async function onRequestGet(context) {
       next, // used for middleware or to fetch assets
       data, // arbitrary space for passing data between middlewares
     } = context;
-  
-    return new Response("Hello, world!!!");
+
+    const lastChecked = await env.IsItBackYet.get('LAST_CHECKED')
+
+    if (lastChecked === null) {
+      return new Response("Value not found", { status: 404 })
+    } else {
+      return new Response(lastChecked)
+    }
 }
+
+/*
+const now = new Date()
+  const lastChecked = Date.parse(localStorage.getItem('lastChecked')) || Date.parse('01 Jan 1970 00:00:00 GMT')
+
+  let timeDiff = now - lastChecked
+  timeDiff /= 1000
+
+  if (timeDiff < 3600) {
+    res.status(429).json({ 
+      errorMessage: 'You already checked less than an hour ago, you can only check every hour.',
+      lastTimeChecked: lastChecked.toISOString()
+    })
+  } else {
+    return fetch('https://www.oat.ie/')    
+      .then(response => response.text())
+      .then(text => {
+        if (/coming soon/i.test(text)) {
+          res.status(200).json({ status: 'Still waiting :(' })
+        } else {
+          res.status(200).json({ status: 'Its back!' })
+        }
+
+        localStorage.setItem('lastChecked', now.toISOString())
+      })
+  }
+*/
