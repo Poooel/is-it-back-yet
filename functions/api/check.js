b392c31fcf28ee44b90f470d66e5f7054890b895
Paul Queruel <paul.queruel@protonmail.com>
2022-06-18 17:25:38 +0100

    remove api directory from nextjs

--- functions/api/check.js
+++ functions/api/check.js
@@ -9,5 +9,5 @@ export async function onRequestGet(context) {
       data, // arbitrary space for passing data between middlewares
     } = context;
   
-    return new Response("Hello, world!");
+    return new Response("Hello, world!!!");
 }
