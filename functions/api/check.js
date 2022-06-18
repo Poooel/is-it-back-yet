9c6d921e5180bfdd8070d53be51e1daceeda8ef3
Paul Queruel <paul.queruel@protonmail.com>
2022-06-18 18:57:24 +0100

    more logging

--- functions/api/check.js
+++ functions/api/check.js
@@ -9,7 +9,7 @@ export async function onRequestGet(context) {
       data, // arbitrary space for passing data between middlewares
     } = context;
 
-    return new Response(JSON.stringify(env))
+    return new Response(JSON.stringify(env.LAST_CHECKED))
 
     let lastCheckedFromKV = await env.LAST_CHECKED.get('LAST_CHECKED')
     let lastChecked
