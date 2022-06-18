3a9d99f77306f4a25b11a4f3445f8868714d5ee3
Paul Queruel <paul.queruel@protonmail.com>
2022-06-18 18:28:03 +0100

    remove try/catch for sentry

--- functions/api/check.js
+++ functions/api/check.js
@@ -9,42 +9,38 @@ export async function onRequestGet(context) {
       data, // arbitrary space for passing data between middlewares
     } = context;
 
-    try {
-      let lastCheckedFromKV = await env.IsItBackYet.get('LAST_CHECKED')
-      let lastChecked
+    let lastCheckedFromKV = await env.IsItBackYet.get('LAST_CHECKED')
+    let lastChecked
 
-      if (lastCheckedFromKV === null) {
-        lastChecked = new Date()
-      } else {
-        lastChecked = new Date(lastCheckedFromKV)
-      }
-  
-      const now = new Date()
-  
-      let timeDiff = now - lastChecked
-      timeDiff /= 1000
-  
-      if (timeDiff < 3600) {
-        return new Response(JSON.stringify({ 
-          errorMessage: 'You already checked less than an hour ago, you can only check every hour.',
-          lastTimeChecked: lastChecked.toISOString()
-        }), { status: 429 })
-      } else {
-        const body = await fetch('https://www.oat.ie/')    
-          .then(response => response.text())
-          .then(text => {
-            if (/coming soon/i.test(text)) {
-              { status: 'Still waiting :(' }
-            } else {
-              { status: "It's back!" }
-            }
-          })
-  
-        await env.IsItBackYet.put('LAST_CHECKED', lastChecked.toISOString())
-  
-        return new Response(JSON.stringify(body))
-      }
-    } catch(e) {
-      return new Response(JSON.stringify(e), { status: 500 })
+    if (lastCheckedFromKV === null) {
+      lastChecked = new Date()
+    } else {
+      lastChecked = new Date(lastCheckedFromKV)
+    }
+
+    const now = new Date()
+
+    let timeDiff = now - lastChecked
+    timeDiff /= 1000
+
+    if (timeDiff < 3600) {
+      return new Response(JSON.stringify({ 
+        errorMessage: 'You already checked less than an hour ago, you can only check every hour.',
+        lastTimeChecked: lastChecked.toISOString()
+      }), { status: 429 })
+    } else {
+      const body = await fetch('https://www.oat.ie/')    
+        .then(response => response.text())
+        .then(text => {
+          if (/coming soon/i.test(text)) {
+            { status: 'Still waiting :(' }
+          } else {
+            { status: "It's back!" }
+          }
+        })
+
+      await env.IsItBackYet.put('LAST_CHECKED', lastChecked.toISOString())
+
+      return new Response(JSON.stringify(body))
     }
 }
