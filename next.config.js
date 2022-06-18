8b2657100e0591d1a2453aa406d6e4a35b2c6a03
Paul Queruel <paul.queruel@protonmail.com>
2022-06-18 17:23:48 +0100

    Try one more thing?

--- next.config.js
+++ next.config.js
@@ -1,14 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  async rewrites() {
-    return [
-      {
-        source: '/check',
-        destination: '/api/test-check'
-      }
-    ]
-  }
 }
 
 module.exports = nextConfig
